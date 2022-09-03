74ce1a1966638ef3ab29598d42e7c039107d3cf9
Roshan Paudel <rozanpoudel@gmail.com>
2022-09-03 15:46:08 +0545

    initial commits

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Header from './components/Header';
+function App() {
+  return (
+    <div className='container'>
+      <Header text={'Feedback UI'} />
+    </div>
+  );
+}
+
+export default App;
