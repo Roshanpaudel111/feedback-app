7d91ce0e30679104f99228269c2cfd9990251dce
Roshan Paudel <rozanpoudel@gmail.com>
2022-09-04 07:46:14 +0545

    made few components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from './components/Header';
+import FeedbackData from './data/feedbackData';
+import { useState } from 'react';
+import FeedbackList from './components/FeedbackList';
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <div className='container'>
       <Header text={'Feedback UI'} />
+      <FeedbackList feedback={feedback} />
     </div>
   );
 }
---
+++ src/data/feedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem amet consectetur adipisicing elit. Debitis consectetur fugit aliquam ut nemo nihil delectus! Illum earum ipsam expedita.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis consectetur fugit aliquam ut nemo nihil delectus! Illum ear.',
+  },
+  {
+    id: 1,
+    rating: 8,
+    text: 'Lorem ipsupisicing elit. Debitis consectetur fugit aliquam ut nemo nihil delectus! Illum earum ipsam expedita.',
+  },
+];
+
+export default FeedbackData;
